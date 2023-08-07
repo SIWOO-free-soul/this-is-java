249671b40a49671f62a8d4b42475cba9029037d9
김시우 <siwoo.kim@kurlycorp.com>
2023-08-07 19:05:55 +0900

    no message

--- src/ch02/VariableUseExample.java
+++ src/ch02/VariableUseExample.java
@@ -5,7 +5,7 @@ public class VariableUseExample {
   public static void main(String[] args) throws Exception {
     int hour = 3;
     int minute = 5;
-
+    int sec;
     System.out.println( hour + "시간" + minute + "분");
   }
 }
